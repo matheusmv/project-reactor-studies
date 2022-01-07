05102e214660cdcf2cabef957e056401f0f1ba61
Matheus Matias <matheus.matias.viana@hotmail.com>
2022-01-07 14:51:01 -0300

    flux - subscribeOn

---
+++ src/test/java/com/github/matheusmv/reactive/test/OperatorsTest.java
@@ -0,0 +1,30 @@
+package com.github.matheusmv.reactive.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+import reactor.test.StepVerifier;
+
+@Slf4j
+public class OperatorsTest {
+
+    @Test
+    public void subscribeOnSimple() {
+        var flux = Flux.range(1, 4)
+                .map(i -> {
+                    log.info("Map 1 - Number {} on Thread {}", i, Thread.currentThread().getName());
+                    return i;
+                })
+                .subscribeOn(Schedulers.single())
+                .map(i -> {
+                    log.info("Map 2 - Number {} on Thread {}", i, Thread.currentThread().getName());
+                    return i;
+                });
+
+        StepVerifier.create(flux)
+                .expectSubscription()
+                .expectNext(1, 2, 3, 4)
+                .verifyComplete();
+    }
+}
