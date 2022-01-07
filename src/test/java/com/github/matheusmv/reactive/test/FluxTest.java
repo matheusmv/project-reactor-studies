9b8d4132448df0100b0d30a988bd56b450d26c8a
Matheus Matias <matheus.matias.viana@hotmail.com>
2022-01-07 12:57:14 -0300

    flux - subscribing and backpressure

--- src/test/java/com/github/matheusmv/reactive/test/FluxTest.java
+++ src/test/java/com/github/matheusmv/reactive/test/FluxTest.java
@@ -2,8 +2,12 @@ package com.github.matheusmv.reactive.test;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+
+import java.util.List;
 
 @Slf4j
 public class FluxTest {
@@ -24,5 +28,106 @@ public class FluxTest {
         );
 
         StepVerifier.create(flux).expectNext(1, 2, 3, 4, 5).verifyComplete();
+    }
+
+    @Test
+    public void fluxSubscriberFromList() {
+        var listOfIntegers = List.of(1, 2, 3, 4, 5);
+        var flux = Flux.fromIterable(listOfIntegers);
+
+        flux.subscribe(
+                i -> log.info("List {}", i)
+        );
+
+        StepVerifier.create(flux).expectNext(1, 2, 3, 4, 5).verifyComplete();
+    }
+
+    @Test
+    public void fluxSubscriberNumbersError() {
+        var flux = Flux.range(1, 5)
+                .map(i -> {
+                    if (i == 4) {
+                        throw new IndexOutOfBoundsException("index error");
+                    }
+
+                    return i;
+                });
+
+        flux.subscribe(
+                i -> log.info("Number {}", i),
+                Throwable::printStackTrace,
+                () -> log.info("finished.")
+        );
+
+        StepVerifier.create(flux)
+                .expectNext(1, 2, 3)
+                .expectError(IndexOutOfBoundsException.class)
+                .verify();
+    }
+
+    @Test
+    public void fluxSubscriberNumbersBackpressure() {
+        var flux = Flux.range(1, 5)
+                .map(i -> {
+                    if (i == 4) {
+                        throw new IndexOutOfBoundsException("index error");
+                    }
+
+                    return i;
+                });
+
+        flux.subscribe(
+                i -> log.info("Number {}", i),
+                Throwable::printStackTrace,
+                () -> log.info("finished."),
+                subscription -> subscription.request(3)
+        );
+
+        StepVerifier.create(flux)
+                .expectNext(1, 2, 3)
+                .expectError(IndexOutOfBoundsException.class)
+                .verify();
+    }
+
+    @Test
+    public void fluxSubscriberNumbersUglyBackpressure() {
+        var flux = Flux.range(1, 10).log();
+
+        flux.subscribe(
+                new Subscriber<Integer>() {
+                    private int count = 0;
+                    private final int requestCount = 2;
+                    private Subscription subscription;
+
+                    @Override
+                    public void onSubscribe(Subscription s) {
+                        this.subscription = s;
+                        subscription.request(requestCount);
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        count++;
+                        if (count >= 2) {
+                            count = 0;
+                            subscription.request(requestCount);
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                }
+        );
+
+        StepVerifier.create(flux)
+                .expectNext(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+                .verifyComplete();
     }
 }
